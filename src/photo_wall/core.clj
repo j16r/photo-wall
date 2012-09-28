6e8ca3edebbb4e86acb84c90ea6a33fcaa9fe158
John Barker <jebarker@gmail.com>
2012-09-28 12:47:46 -0400

    Initial commit of Photo Wall

---
+++ src/photo_wall/core.clj
@@ -0,0 +1,1 @@
+(ns photo-wall.core)
---
+++ test/photo_wall/test/core.clj
@@ -0,0 +1,6 @@
+(ns photo-wall.test.core
+  (:use [photo-wall.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
